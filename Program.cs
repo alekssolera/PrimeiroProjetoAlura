9e475c0edb2e9fba0be41a6abda90320726e96f3
Alex Nascimento Solera <nalex8130@gmail.com>
2025-08-04 15:46:18 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿string mensagemDeBoasVindas = "Bem-vindo ao Zynk Music";
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(@"    
+█░▄▄░▄█▄─█─▄█▄─▀█▄─▄█▄─█─▄███▄─▀█▀─▄█▄─██─▄█─▄▄▄▄█▄─▄█─▄▄▄─█
+██▀▄█▀██▄─▄███─█▄▀─███─▄▀█████─█▄█─███─██─██▄▄▄▄─██─██─███▀█
+▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▄▀▀▄▄▀▄▄▀▄▄▀▀▀▄▄▄▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▄▀▄▄▄▄▄▀
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+ 
+}
+
+void ExibirMenu()
+{
+    Console.WriteLine("\nSelecione uma opção:");
+    Console.WriteLine("Digite 1 para resgistrar uma banda ou individual");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para mostrar as avaliações da banda");
+    Console.WriteLine("\nSAÍDA");
+    Console.WriteLine("Digite -1 para sair:");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: RegistrarBandas();
+            break;
+        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case -1:
+            Console.WriteLine("Saindo do programa...");
+            Environment.Exit(0);
+            break;
+        default: Console.WriteLine("Opção inválida. Por favor, tente novamente.");
+            ExibirMenu();
+            break;
+    }
+}
+
+void RegistrarBandas()
+{
+    Console.Clear();
+    Console.WriteLine("Registri de Bandas");
+}
+ExibirMensagemDeBoasVindas();
+ExibirMenu();
